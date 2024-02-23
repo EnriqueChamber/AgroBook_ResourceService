d527255e180b731c365c56dc4c7e0abbc7465ae1
EnriqueChamber <kike98chgq@gmail.com>
2024-02-23 14:48:52 +0100

    conectado con FrontEnd. Habilitado CrossOrigine en controlador de Explotaciones

--- src/main/java/es/agrobook/api/controller/ExplotacionController.java
+++ src/main/java/es/agrobook/api/controller/ExplotacionController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import es.agrobook.api.model.Explotacion;
 import es.agrobook.api.service.ExplotacionService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class ExplotacionController {
