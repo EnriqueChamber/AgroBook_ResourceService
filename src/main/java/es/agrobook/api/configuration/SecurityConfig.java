2632a91f1ba5eaa854396c638584da7da356b36d
EnriqueChamber <kike98chgq@gmail.com>
2024-02-26 11:25:47 +0100

    Update SecurityConfig.java

--- src/main/java/es/agrobook/api/configuration/SecurityConfig.java
+++ src/main/java/es/agrobook/api/configuration/SecurityConfig.java
@@ -1,4 +1,6 @@
 package es.agrobook.api.configuration;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -6,6 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -15,16 +20,25 @@ public class SecurityConfig {
     // Refs.: https://stackoverflow.com/questions/56864133/how-to-detect-exactly-if-authentication-fail-because-username-or-password-incorr
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors(Customizer.withDefaults())
-            .authorizeHttpRequests((authz) -> authz
+        http.cors(Customizer.withDefaults());
+        http.authorizeHttpRequests((authz) -> authz
                 .anyRequest().authenticated()
-            )
-			.httpBasic(Customizer.withDefaults())
-			.formLogin(Customizer.withDefaults());
+            );
+        http.httpBasic(Customizer.withDefaults());
+        http.formLogin(Customizer.withDefaults());
+        //http.oauth2ResourceServer(auth -> auth.jwt(Customizer.withDefaults()));
         return http.build();
     }
 
-    
+    /*@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "HEAD"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }*/
     /*@Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers("/ignore1", "/ignore2");
